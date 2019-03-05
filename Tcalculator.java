a8596eb1e78ce92f828912c09199fcd1f0d71282
deeptishakya20 <48085127+deeptishakya20@users.noreply.github.com>
2019-03-05 16:23:41 +0530

    Added Square Function

---
+++ Tcalculator.java
@@ -0,0 +1,64 @@
+public class Calculator{
+  
+
+    public  static void main (String [] args){
+     
+        System.out.println("Helllo World!!!");
+     
+            System.out.println("Calculator java program");
+   
+        }
+  
+        public  static void Addition (int number1, int number2){
+   
+          System.out.println("HAddition="+(number1+number2));
+
+
+           }
+
+    
+              public  static void DIVISION (int number1, int number2){
+    
+                if(number2==0)
+    
+                 System.out.println("division should not be 0");
+    
+                else
+   
+
+            System.out.println("division="+(number1/number2) );
+
+  
+
+            }
+   
+          public  static void Substraction (int number1, int number2){
+     
+        System.out.println("Substraction="+(number1-number2));
+  
+
+        }
+  
+          public  static void Multiplicatiion (int number1, int number2){
+   
+                  System.out.println("Multiplication="+(number1*number2));
+
+   
+
+
+        }
+
+
+             public  static void Square (int number1){
+   
+                  System.out.println("Square="+(number1*number1));
+
+   
+
+
+        }
+      
+
+
+  
+}
